8db61b0e00bfc6e7c8c790ebf7f94341a44c137b
FurqanGit00 <furqanbusiness0@gmail.com>
2025-02-05 02:33:38 +0500

    Add eighth Project

---
+++ Sum of Digits of a Number/Sum of Digits of a Number.py
@@ -0,0 +1,10 @@
+n = int(input("enter a number to add its digits:"))
+sum = 0
+while n>0:
+    sum = sum + n%10
+    n = n//10
+print("sum of digits are", sum)
+
+
+
+#zarak bhai ye bohat hard tha kutton ki tarha copy pr banaya phir likha vscode pr"
