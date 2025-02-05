cf97b64926fc4f5666d7a4850b398109f76fb2de
FurqanGit00 <furqanbusiness0@gmail.com>
2025-02-05 19:30:52 +0500

    Add Fourteenth Project

---
+++ Check if number is palindrome/check if number is palindrome.py
@@ -0,0 +1,11 @@
+take_input = int(input("Enter a palindrome:"))
+
+reversed_str = take_input[::-1]
+
+if(reversed_str == take_input):
+ 
+    print("the string is palindrome")
+
+else:
+ 
+    print("the string is not palindrome")    
