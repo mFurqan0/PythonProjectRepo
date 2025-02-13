e4d8430ba0adbf45f6ac56f644d9d504381424a9
FurqanGit00 <furqanbusiness0@gmail.com>
2025-02-14 02:09:46 +0500

    Class&Objects

---
+++ Class&Object/Class&Object.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def  get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("hi", self.name, "your avg score is:", sum/3)    
+
+
+s1 = Student("Furqan", [98,75,68])
+s1.get_avg()
+
+
