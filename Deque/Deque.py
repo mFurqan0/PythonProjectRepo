b2b68c9cc34252bf5df9919f89f1b95360b9ff69
FurqanGit00 <furqanbusiness0@gmail.com>
2025-02-19 03:55:13 +0500

    Deque

--- Deque/Deque.py
+++ Deque/Deque.py
@@ -1,37 +1,16 @@
-# importing "collections" for deque operations
-import collections
+from collections import deque  
 
-# initializing deque
-de = collections.deque([1,2,3])
+def is_palindrome(s):  
+    # Clean the string: lowercase and remove spaces  
+    cleaned = s.lower().replace(" ", "")  
+    dq = deque(cleaned)  
 
-# using append() to insert element at right end
-# inserts 4 at the end of deque
-de.append(4)
+    while len(dq) > 1:  
+        left = dq.popleft()  
+        right = dq.pop()  
+        if left != right:  
+            return False  
+    
+    return True
 
-# printing modified deque
-print("The deque after appending at right is : ")
-print(de)
-
-# using appendleft() to insert element at left end
-# inserts 6 at the beginning of deque
-de.appendleft(6)
-
-# printing modified deque
-print("The deque after appending at left is : ")
-print(de)
-
-# using pop() to delete element from right end
-# deletes 4 from the right end of deque
-de.pop()
-
-# printing modified deque
-print("The deque after deleting from right is : ")
-print(de)
-
-# using popleft() to delete element from left end
-# deletes 6 from the left end of deque
-de.popleft()
-
-# printing modified deque
-print("The deque after deleting from left is : ")
-print(de)
+print(is_palindrome("Racecar"))  
