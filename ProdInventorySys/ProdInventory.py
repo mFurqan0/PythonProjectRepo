a4d151360cca304e06dfc09833ba1e91557101cf
FurqanGit00 <furqanbusiness0@gmail.com>
2025-02-19 02:13:42 +0500

    New change is file

--- ProdInventorySys/ProdInventory.py
+++ ProdInventorySys/ProdInventory.py
@@ -1,25 +1,5 @@
 class Product:
     def __init__(self, name, price, quantity):
-<<<<<<< HEAD
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-    
-    def update_stock(self, sold_quantity):
-        self.quantity -= sold_quantity
-    
-    def get_total_value(self):
-        return self.price * self.quantity
-    
-    def display(self):
-        print(f"{self.name} - Price: ${self.price}, Stock: {self.quantity}")
-
-# Test Case:
-p1 = Product("Iphone14", 350, 56)
-p1.update_stock(12)
-p1.display()
-print("Total Inventory Value:", p1.get_total_value())
-=======
         self.name  = name 
         self.price = price 
         self.quantity = quantity
@@ -43,4 +23,3 @@ p1 = Product("Iphone14",350,56)
 p1.update_stock(12)
 p1.display()
 print("Total value of Inventory:",p1.get_total_value())
->>>>>>> ec5ad5aa70dd1fd5c6bc622741e7e69fddc63da5
