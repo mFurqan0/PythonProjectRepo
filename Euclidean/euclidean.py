3e035f68eaf201e4d4a7ad0b75784b595a70416c
mFurqan0 <furqanbusiness0@gmail.com>
2025-03-13 08:47:23 +0500

    add

---
+++ Euclidean/euclidean.py
@@ -0,0 +1,12 @@
+import math
+
+def main():
+    x = float(input("Enter value of x: "))
+    y = float(input("Enter value of y: "))
+    
+    distance = math.sqrt(x * x + y * y)
+    
+    print("Euclidean distance between X and Y is:", distance)
+
+if __name__ == "__main__":
+    main()
